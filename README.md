3d3f980ef4ff2a8f04e94df7ae3994431d3de3e1
Ann Levina <annakarkhanina@mail.ru>
2023-09-15 17:36:02 +0400

    add tests and README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # qa_python
+
+Список тестов:
+test_add_new_book_add_search_in_list - проверяем, что книга добавлена в список books_genre
+test_set_book_genre_add_search_in_directory - проверяем, что жанр добавлен в список
+test_get_book_genre_assign_the_genre_correctly - проверяем, что книге присвоен жанр
+test_get_books_with_specific_genre_successfully - проверяем, что функция выводит список книг по жанру
+test_get_books_with_specific_genre_extra_genre_not_added - проверка, что книги другого жанра не попападают в вывод функции
+test_get_books_genre_the_correct_list_is_displayed - проверяем, что в словаре добавлены имя фильма и жанр
+test_get_books_for_children_horror_is_not_on_the_list - проверяем, что ужасы не добавлены в список для детей
+test_get_books_for_children_adding_to_the_list_of_desired_genres - проверяем, что добавление в список для детей проходит
+test_add_book_in_favorites_successfully_added - проверяем, что книги добавляются в избранное
+test_delete_book_from_favorites_successfully_deleted - проверяем, что книги удаляются из избранного
+test_get_list_of_favorites_books_successful_output_of_a_list_of_favorite_books - проверяем получения списка избранного
+
+
--- tests.py
+++ tests.py
@@ -1,24 +1,124 @@
+import pytest
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    #проверяем, что книга добавлена в список books_genre
+    def test_add_new_book_add_search_in_list(self):
 
-        # добавляем две книги
+        collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert 'Гордость и предубеждение и зомби' in collector.books_genre
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    #проверяем, что жанр добавлен в список
+    def test_set_book_genre_add_search_in_directory(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
+
+        assert 'Фантастика' in collector.books_genre.values()
+
+    #проверяем, что книге присвоен жанр
+    def test_get_book_genre_assign_the_genre_correctly(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
+
+        assert collector.get_book_genre('Гордость и предубеждение и зомби') == 'Фантастика'
+
+    #проверяем, что функция выводит список книг по жанру
+    def test_get_books_with_specific_genre_successfully(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Гордость и предубеждение и зомби','Фантастика')
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер', 'Фантастика')
+        collector.add_new_book('Король Лев')
+        collector.set_book_genre('Король Лев', 'Мультфильмы')
+
+        assert ['Гордость и предубеждение и зомби', 'Гарри Поттер'] == collector.get_books_with_specific_genre('Фантастика')
+
+    #проверка, что книги другого жанра не попападают в вывод функции
+    def test_get_books_with_specific_genre_extra_genre_not_added(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Гордость и предубеждение и зомби','Фантастика')
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер', 'Фантастика')
+        collector.add_new_book('Король Лев')
+        collector.set_book_genre('Король Лев', 'Мультфильмы')
+
+        assert ['Гордость и предубеждение и зомби', 'Король Лев'] != collector.get_books_with_specific_genre('Фантастика')
+
+    #проверяем, что в словаре добавлены имя фильма и жанр
+    def test_get_books_genre_the_correct_list_is_displayed(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
+        assert collector.get_books_genre() == {'Гордость и предубеждение и зомби': 'Фантастика'}
+
+    #проверяем, что ужасы не добавлены в список для детей
+    def test_get_books_for_children_horror_is_not_on_the_list(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Кошмар на улице Вязов')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
+        collector.set_book_genre('Кошмар на улице Вязов', 'Ужасы')
+        assert 'Кошмар на улице Вязов' not in collector.get_books_for_children()
+
+    #проверяем, что добавление в список для детей проходит
+    def test_get_books_for_children_adding_to_the_list_of_desired_genres(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Кошмар на улице Вязов')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
+        collector.set_book_genre('Кошмар на улице Вязов', 'Ужасы')
+        assert len(collector.get_books_for_children()) == 1
+
+    # проверяем, что книги добавляются в избранное
+    @pytest.mark.parametrize(
+        'name',
+        (
+                'Гордость и предубеждение и зомби',
+                'Гарри Поттер',
+                'Король Лев'
+        )
+    )
+    def test_add_book_in_favorites_successfully_added(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        assert collector.favorites
+
+    #проверяем, что книги удаляются из избранного
+    @pytest.mark.parametrize(
+        'name',
+        (
+                'Гордость и предубеждение и зомби',
+                'Гарри Поттер'
+        )
+    )
+    def test_delete_book_from_favorites_successfully_deleted(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        collector.delete_book_from_favorites(name)
+        assert collector.favorites == []
+
+    #проверяем получения списка избранного
+    def test_get_list_of_favorites_books_successful_output_of_a_list_of_favorite_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        assert collector.favorites == ['Гордость и предубеждение и зомби']
+
